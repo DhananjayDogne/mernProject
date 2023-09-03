7ff56c17d04095825e1f369e80f078b4d358e3a8
dhananjay <dhananjaydogne@gmail.com>
2023-09-03 17:47:38 +0530

    adding all the files

---
+++ components/PageItem.jsx
@@ -0,0 +1,59 @@
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { MdOutlineVisibility } from 'react-icons/md';
+import { BsFillShareFill } from 'react-icons/bs';
+
+const PageItem = () => {
+    return (
+        <>
+            <div className="card text-start m-auto mb-1"  >
+                <img src="https://s3-alpha-sig.figma.com/img/8f0b/5f52/59515bfc8d2d4ff8928be097376153e9?Expires=1694390400&Signature=bLojQqf9tAn~PFapGnD1FUhLmHCTm~E~hWFmWvSWt~awqr8aDmWmvR-fZP3cpZkKu3y3pmlkRuH6fjYR4Go2KbEphmKI9a936InoVwbNoWUkGLNfYu4YbNlFJbeh1DD-VBHWbEtEspWUT4I1UNCE006LlwqlfW0uQzrR11WZoJsnlszCqH9tHNGvyqIFlEoHHgXfl-iJnoaeh-wjWCPyJL7mI5xWN46blHyQHm3cCBCWUqkMzENjQxDVMeWRRkASw-WJ4WJ1EcexnLNHyUfh3DVCckuP1BLv0DHPgyrmD~ghmPLojQ4F8nwhP0lG8NvKg3XVFW0YxZDt-LnPTF9CXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" className="card-img-top" style={{ height: "15rem" }} alt="..." />
+                <div className="card-body ">
+                    <header className="fw-bold fs-6 py-1">✍️ Article</header>
+                    <div className="d-flex">
+
+                        <p className='fw-bolder fs-5 '> What if famous brands had regular fonts? Meet RegulaBrands!</p>
+                        <FiMoreHorizontal style={{ width: "5rem", height: "2rem" }} />
+                    </div>
+                    <p className='fw-light'>
+                        I've worked in UX for the better part of a decade. From now on, I plan to rei…
+
+                    </p>
+                    <div className=' d-flex '>
+                        <img className='rounded-circle ' style={{ width: "30px" }} src="https://s3-alpha-sig.figma.com/img/d809/8d9d/f42d73f4b2b7835b4c918c4edcf10f64?Expires=1694390400&Signature=CfH2E3gUi4MkVsAH5OwL6njwo0TjgiHfSClqpl1z7Kr4oK9nLm4Cu5X~vBL4kuHB7yHKOC~x7V1Pa0Fn1Yt38XzN7hqBeFvp9cldNu6ie4E8hvP3-WtMAzU7i4lxzEhZAjDlzZIPFktWyChiUDwNX~T3r7DKoNlp~EZ4qVCraZoCVEm6zSd2PfMczDrtr-vjH83e0XoVyGQvNY5tNkPyeGsCzTdKSW8wzNIcDZAdPMBQ~moYpFPTTtgqqZsaWIFw2gU1Tpmg9R8kmEasRUDPP4AR3BXOBw4yS7Cbk4TTr92bLlS8Icqb9H29Xj7kh1dSQsVxooPIoMkrOdPzk0CgQw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+                        <h6 className='my-auto ms-1'>Sarthak Kamra</h6>
+                        <div className='ms-auto fs-6' style={{ fontSize: "10px" }}>
+                            <MdOutlineVisibility />
+                            <span className='mx-3 fw-light'>1.4k views</span>
+                            <BsFillShareFill className=' p-1 fs-4' style={{ backgroundColor: "#EDEEF0" }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="card text-start m-auto mb-1"  >
+                <img src="https://s3-alpha-sig.figma.com/img/8f0b/5f52/59515bfc8d2d4ff8928be097376153e9?Expires=1694390400&Signature=bLojQqf9tAn~PFapGnD1FUhLmHCTm~E~hWFmWvSWt~awqr8aDmWmvR-fZP3cpZkKu3y3pmlkRuH6fjYR4Go2KbEphmKI9a936InoVwbNoWUkGLNfYu4YbNlFJbeh1DD-VBHWbEtEspWUT4I1UNCE006LlwqlfW0uQzrR11WZoJsnlszCqH9tHNGvyqIFlEoHHgXfl-iJnoaeh-wjWCPyJL7mI5xWN46blHyQHm3cCBCWUqkMzENjQxDVMeWRRkASw-WJ4WJ1EcexnLNHyUfh3DVCckuP1BLv0DHPgyrmD~ghmPLojQ4F8nwhP0lG8NvKg3XVFW0YxZDt-LnPTF9CXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" className="card-img-top" style={{ height: "15rem" }} alt="..." />
+                <div className="card-body ">
+                    <header className="fw-bold fs-6 py-1">✍️ Article</header>
+                    <div className="d-flex">
+
+                        <p className='fw-bolder fs-5 '> What if famous brands had regular fonts? Meet RegulaBrands!</p>
+                        <FiMoreHorizontal style={{ width: "5rem", height: "2rem" }} />
+                    </div>
+                    <p className='fw-light'>
+                        I've worked in UX for the better part of a decade. From now on, I plan to rei…
+
+                    </p>
+                    <div className=' d-flex '>
+                        <img className='rounded-circle ' style={{ width: "30px" }} src="https://s3-alpha-sig.figma.com/img/d809/8d9d/f42d73f4b2b7835b4c918c4edcf10f64?Expires=1694390400&Signature=CfH2E3gUi4MkVsAH5OwL6njwo0TjgiHfSClqpl1z7Kr4oK9nLm4Cu5X~vBL4kuHB7yHKOC~x7V1Pa0Fn1Yt38XzN7hqBeFvp9cldNu6ie4E8hvP3-WtMAzU7i4lxzEhZAjDlzZIPFktWyChiUDwNX~T3r7DKoNlp~EZ4qVCraZoCVEm6zSd2PfMczDrtr-vjH83e0XoVyGQvNY5tNkPyeGsCzTdKSW8wzNIcDZAdPMBQ~moYpFPTTtgqqZsaWIFw2gU1Tpmg9R8kmEasRUDPP4AR3BXOBw4yS7Cbk4TTr92bLlS8Icqb9H29Xj7kh1dSQsVxooPIoMkrOdPzk0CgQw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+                        <h6 className='my-auto ms-1'>Sarthak Kamra</h6>
+                        <div className='ms-auto fs-6' style={{ fontSize: "10px" }}>
+                            <MdOutlineVisibility />
+                            <span className='mx-3 fw-light'>1.4k views</span>
+                            <BsFillShareFill className=' p-1 fs-4' style={{ backgroundColor: "#EDEEF0" }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default PageItem;
